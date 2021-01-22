15273879b6d99631ad1396d8d6d89b8ae05d55c7
IgorChuvaev <strickle@mail.ru>
2021-01-22 20:33:14 +0300

    added images

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const products = [
-  { id: 1, title: 'Notebook', price: 20000, img: 'images/1.jpg' },
-  { id: 2, title: 'Mouse', price: 1500, img: 'images/2.jpg' },
-  { id: 3, title: 'Keyboard', price: 5000, img: 'images/3.jpg' },
-  { id: 4, title: 'Gamepad', price: 4500, img: 'images/4.jpg' },
+  { id: 1, title: 'Ballantines', price: 1500, img: 'images/1.jpg' },
+  { id: 2, title: 'Jameson', price: 1500, img: 'images/2.jpg' },
+  { id: 3, title: 'MACALLAN', price: 3500, img: 'images/3.jpg' },
+  { id: 4, title: 'Jack Daniels', price: 2000, img: 'images/4.jpg' },
 ];
 
 const renderProduct = (title, price, img) => {
@@ -16,7 +16,7 @@ const renderProduct = (title, price, img) => {
 
 const renderProducts = (list) => {
   const productList = list.map((product) =>
-    renderProduct(product.title, product.price)
+    renderProduct(product.title, product.price, product.img)
   );
   document.querySelector('.products').innerHTML = productList.join('');
 };
