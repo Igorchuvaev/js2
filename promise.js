c31ad70fefcc11f0cf5c96573cbe130882fd19d3
IgorChuvaev <strickle@mail.ru>
2021-02-01 10:16:52 +0300

    promise

---
+++ promise.js
@@ -0,0 +1,9 @@
+function makeGETRequest(url, callback) {
+    return new Promise((resolve, reject) => {
+                let xhr = window.XMLHttpRequest ? new window.XMLHttpRequest() : new window.ActiveXObject;
+        xhr.open("GET", url, true);
+        xhr.onload = () => resolve(callback(xhr.responseText));
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send();
+      });
+}
